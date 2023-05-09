b8acb9314f019450932bbc315bcb1e1207d6dacb
Anna Dobina <anna-dobina@live.nmit.ac.nz>
2023-05-10 10:03:59 +1200

    Challenge 1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+let obj = {
+    width: 2,
+    length: 5,
+    height: 1
+}
+
+
+
+function volumeOfContainer(width, length, height) {
+    return obj.width*obj.length*obj.height;
+}
+console.log(volumeOfContainer())
