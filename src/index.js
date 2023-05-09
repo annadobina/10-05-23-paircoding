aabf97da906a29336bb28bc447d8ffe7ac999448
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-05-10 09:51:34 +1200

    Fixing the unsynced files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let obj = {
+    width: 2,
+    length: 5,
+    height: 1
+};
+
+function volumeOfContainer(obj.[width], obj.[length], obj.[height]) {
+    return obj.width*obj.length*obj.height;
+}
+console.log(volumeOfContainer())
