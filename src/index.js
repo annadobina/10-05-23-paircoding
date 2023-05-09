1e46013d93afb88f0836cb05fb28de66b78c7c02
Anna Dobina <anna-dobina@live.nmit.ac.nz>
2023-05-10 11:59:45 +1200

    Challenge 3

--- src/index.js
+++ src/index.js
@@ -13,15 +13,52 @@
 
 
 
-let obj = {
-    a:1,
-    b:2,
-    c:3
+// let obj = {
+//     a:1,
+//     b:2,
+//     c:3
+// }
+// // let output = Object.entries(obj);       this is the easy way
+// // console.log(output);
+// function convertArray (obj){                //this is the function
+//     let output = Object.entries(obj);
+//     return output;
+// }
+// console.log(convertArray(obj))
+
+// function numbersAndLetters (str){
+//     newObj = {
+//         "letters": 0,
+//         "numbers": 0
+//     };
+//     let numCount =0;
+//     for (let i=0; i < str.length; i++){
+//         if (Character.isDigit(str.charAt(i)))
+//         numCount++;
+//     }
+//     let strLength = "";
+//     for (let i=0; i < str.length; i++){
+
+// }
+
+function numbersAndLetters (str){
+    let letters = [];
+    let numbers = [];
+
+    const filterStr = str.split('').filter(item => item !=' ');
+    console.log(filterStr)
+
+    for (let i=0; i< filterStr.length ; i++){
+        if (isNaN(filterStr[i])){
+            letters.push(filterStr[i]);
+        } else {
+         numbers.push(filterStr[i]);
+     }
+    }
+    let obj = {
+        'letters': letters.length,
+        'numbers': numbers.length 
+};
+return  console.log(obj);
 }
-// let output = Object.entries(obj);       this is the easy way
-// console.log(output);
-function convertArray (obj){                //this is the function
-    let output = Object.entries(obj);
-    return output;
-}
-console.log(convertArray(obj))
+console.log(numbersAndLetters('Hello World 2023'));
