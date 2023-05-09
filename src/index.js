359bfc334b39a5311b27caa33cbb8f73c5e64215
Anna Dobina <anna-dobina@live.nmit.ac.nz>
2023-05-10 10:24:50 +1200

    Challenge 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+// let obj = {
+//     width: 2,
+//     length: 5,
+//     height: 1
+// }
+
+
+
+// function volumeOfContainer(width, length, height) {
+//     return obj.width*obj.length*obj.height;
+// }
+// console.log(volumeOfContainer())
+
+
+
 let obj = {
-    width: 2,
-    length: 5,
-    height: 1
+    a:1,
+    b:2,
+    c:3
 }
-
-
-
-function volumeOfContainer(width, length, height) {
-    return obj.width*obj.length*obj.height;
+// let output = Object.entries(obj);       this is the easy way
+// console.log(output);
+function convertArray (obj){                //this is the function
+    let output = Object.entries(obj);
+    return output;
 }
-console.log(volumeOfContainer())
+console.log(convertArray(obj))
