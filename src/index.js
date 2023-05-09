6e9eaec5cde72ae6918ac92d6a90d06d988fad14
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-05-10 09:56:39 +1200

    Still trying to fix sync

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let obj = {
     height: 1
 };
 
-function volumeOfContainer(obj.[width], obj.[length], obj.[height]) {
+function volumeOfContainer(width, length, height) {
     return obj.width*obj.length*obj.height;
 }
 console.log(volumeOfContainer())
